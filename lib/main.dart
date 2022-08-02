8e662ee0fda51a6a86d8e26fc2458b24219bb3b6
Programmer <123sphangidi@gmail.com>
2022-08-02 19:49:42 +0200

    SlaQa Salon Project Structure

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SlaQa Salon',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Splashscreen(),
+    );
+  }
+}
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
